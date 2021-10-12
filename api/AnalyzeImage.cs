7634618fd22f942b742526bfec473b98ec23435b
Melissa de los Angeles Garro Valverde <cph-mv174@cphbusiness.dk>
2021-10-12 20:50:00 +0200

    added api

---
+++ api/AnalyzeImage.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public static class AnalyzeImage
+    {
+        [FunctionName("AnalyzeImage")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+
+            return new OkObjectResult("This is the returned message");
+        }
+    }
+}
