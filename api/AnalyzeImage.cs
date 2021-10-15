83c774748868d5ed8ebda3ed9250f178c1638bd5
Melissa <g.meliv@gmail.com>
2021-10-15 21:01:47 +0200

    commit html to api

--- api/AnalyzeImage.cs
+++ api/AnalyzeImage.cs
@@ -17,7 +17,8 @@ namespace Company.Function
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-
+            var someObject = new{text= "This is the returned message"};
+             
             return new OkObjectResult("This is the returned message");
         }
     }
